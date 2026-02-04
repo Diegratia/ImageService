5657ec408b83cb99f0a81ca811f841ef45963420
Diegratia <d.febrian2002@gmail.com>
2026-02-04 11:33:18 +0700

    awdad

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const path = require("path");
 const uploadLocal = require("./routes/uploadLocal");
 const uploadSupabase = require("./routes/uploadSupabase");
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", uploadLocal);
